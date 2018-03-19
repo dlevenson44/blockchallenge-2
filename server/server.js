3d9d78bfdace2b68b0d162784ab3de3cd418d73b
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-19 09:21:57 -0600

    set up server.js file without routes

--- server/server.js
+++ server/server.js
@@ -1,18 +1,41 @@
+// import dependencies
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const express = require('express')
+const logger = require('morgan')
+const methodOverride = require('method-override')
 const path = require('path')
 
+// initiate app
 const app = express()
+
+// set up body parser
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+// set cookie parser
+app.use(cookieParser())
+
+// set logger
+app.use(logger('dev'))
+
+// set static files
+app.use(express.static(path.join(__dirname, '../../client/build')))
+
+// set and initiate routes
 const router = express.Router()
-
-const staticFiles = express.static(path.join(__dirname, '../../client/build'))
-app.use(staticFiles)
-
-
 app.use(router)
+
+// set and initiate port
 app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
   console.log(`Listening on ${app.get('port')}`)
+})
+
+// set routes here
+
+
+// set error handler
+app.use('*', (req, res) => {
+	res.status(404).send('Invalid Page')
 })
