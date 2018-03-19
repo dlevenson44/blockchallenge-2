a7f62f54c5401108424b8b94ad72dd34116bd7d9
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-19 09:07:56 -0600

    removed code from old project

--- server/server.js
+++ server/server.js
@@ -10,14 +10,7 @@ const router = express.Router()
 const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 app.use(staticFiles)
 
-router.get('/cities', (req, res) => {
-  const cities = [
-    {name: 'New York City', population: 8175133},
-    {name: 'Los Angeles',   population: 3792621},
-    {name: 'Chicago',       population: 2695598}
-  ]
-  res.json(cities)
-})
+
 app.use(router)
 app.set('port', (process.env.PORT || 3001))
 app.listen(app.get('port'), () => {
