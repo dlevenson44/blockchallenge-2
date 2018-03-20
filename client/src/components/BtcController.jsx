a00143f7db90f9d8db319e00d83d741242eb6b70
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-20 12:21:15 -0600

    added str to num type in btc comp, added substrings

--- client/src/components/BtcController.jsx
+++ client/src/components/BtcController.jsx
@@ -5,6 +5,9 @@ class BtcController extends Component {
     constructor(props) {
         super(props)
         this.state = {
+            dash_usd: 0,
+            eth_usd: 0,
+            ltc_usd: 0,
             usd: 0,
             usHigh: 0,
             usLow: 0,
@@ -27,8 +30,54 @@ class BtcController extends Component {
             fetch('/api/crypto')
             .then(res => res.json())
             .then(res => {
+                // calculate alt per btc values
+                // convert dash_usd to number type
+                let dashContainer = ''
+                let dashValue = res.data.crypto[0].dash_usd
+                for(let i = 0; i < dashValue.length -1; i++) {
+                    // filter commas out of string
+                    if(dashValue[i] !== (",")) {
+                        dashContainer += dashValue[i]
+                    }
+                }
+                // convert eth_usd to number type
+                let ethContainer = ''
+                let ethValue = res.data.crypto[0].eth_usd
+                for(let i = 0; i < ethValue.length -1; i++) {
+                    // filter commas out of string
+                    if(ethValue[i] !== (",")) {
+                        ethContainer += ethValue[i]
+                    }
+                }
+                // convert ltc_usd to number type
+                let ltcContainer = ''
+                let ltcValue = res.data.crypto[0].ltc_usd
+                for(let i = 0; i < ltcValue.length -1; i++) {
+                    // filter commas out of string
+                    if(ltcValue[i] !== (",")) {
+                        ltcContainer += ltcValue[i]
+                    }
+                }
+                // convert btc_usd to number type
+                let btcContainer = ''
+                let btcValue = res.data.crypto[0].btc_usd
+                for(let i = 0; i < btcValue.length -1; i++) {
+                    // filter commas out of string
+                    if(btcValue[i] !== (",")) {
+                        btcContainer += btcValue[i]
+                    }
+                }
+                // convert string to number
+                let dashUsd = parseFloat(dashContainer)
+                let ethUsd = parseFloat(ethContainer)
+                let ltcUsd = parseFloat(ltcContainer)
+                let btcUsd = parseFloat(btcContainer)
+                // set state
                 this.setState({
-                    usd: res.data.crypto[0].btc_usd,
+                    dash_usd: res.data.crypto[0].dash_usd,
+                    eth_usd: res.data.crypto[0].eth_usd,
+                    ltc_usd: res.data.crypto[0].ltc_usd,
+                    usd: btcUsd,
                     usHigh: res.data.crypto[0].btc_us_high,
                     usLow: res.data.crypto[0].btc_us_low,
                     eur: res.data.crypto[0].btc_eur,
@@ -56,12 +105,12 @@ class BtcController extends Component {
                     <p>{this.state.oneWeek}% change in last 7 days</p>                
                 <h5>BTC US Market Info</h5>
                     <p>${this.state.usd} per BTC</p>
-                    <p>${this.state.usHigh} is the 24 hour high</p>
-                    <p>${this.state.usLow} is the 24 hour low</p>                            
+                    <p>${(this.state.usHigh).substring(0, 8)} is the 24 hour high</p>
+                    <p>${(this.state.usLow).substring(0, 8)} is the 24 hour low</p>                            
                 <h5>BTC EU Market Info</h5>
-                    <p>€{this.state.eur} per BTC</p>
-                    <p>€{this.state.eurHigh} is the 24 hour high</p>
-                    <p>€{this.state.eurLow} is the 24 hour low</p>            
+                    <p>€{(this.state.eur).substring(0, 8)} per BTC</p>
+                    <p>€{(this.state.eurHigh).substring(0, 8)} is the 24 hour high</p>
+                    <p>€{(this.state.eurLow).substring(0, 8)} is the 24 hour low</p>            
             </div>
             )
 
