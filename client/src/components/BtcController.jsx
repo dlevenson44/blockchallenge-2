abbef6ae8ad5d89257ec4a00c555d95b5ad45481
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-20 10:02:19 -0600

    fetching data into btcocntroller state

--- client/src/components/BtcController.jsx
+++ client/src/components/BtcController.jsx
@@ -24,7 +24,19 @@ class BtcController extends Component {
         fetch('/api/crypto')
         .then(res => res.json())
         .then(res => {
-            console.log(res)
+            this.setState({
+                usd: res.data.crypto[0].btc_usd,
+                usHigh: res.data.crypto[0].btc_us_high,
+                usLow: res.data.crypto[0].btc_us_low,
+                eur: res.data.crypto[0].btc_eur,
+                eurHigh: res.data.crypto[0].btc_eur_high,
+                eurLow: res.data.crypto[0].btc_eur_low,
+                trades: res.data.crypto[0].btc_trades,
+                oneHour: res.data.crypto[0].btc_one_hour,
+                oneDay: res.data.crypto[0].btc_24_hours,
+                oneWeek: res.data.crypto[0].btc_7_days,
+                fetchStatus: true,
+            })
         })
         .catch(err => console.log(err))
     }
