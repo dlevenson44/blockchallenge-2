0f87e5086de72b6a604bca87ab3ccd650c9856b8
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-20 13:07:29 -0600

    set up Nav component

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Nav = () => {
+    return(
+        <div>
+            <div className="hidden-md-down" id="nav-container">
+                <ul className="nav justify-content-center">
+                <li className="nav-item" id="nav-li">                    
+                    <Link to="/blockchallenge-1" className="nav link" id="nav-spec">Home</Link>
+                </li>
+
+                <li className="nav-item" id="nav-li">                    
+                    <Link to="/bitcoin" className="nav link" id="nav-spec">BitCoin</Link>
+                </li>
+
+                <li className="nav-item" id="nav-li">                    
+                    <Link to="/dash" className="nav link" id="nav-spec">DASH</Link>
+                </li>
+             
+
+                <li className="nav-item" id="nav-li">                    
+                    <Link to="/ethereum" className="nav link" id="nav-spec">Ethereum</Link>
+                </li>
+
+                <li className="nav-item" id="nav-li">                    
+                    <Link to="/litecoin" className="nav link" id="nav-spec">LiteCoin</Link>
+                </li>                                                  
+
+                </ul>
+            </div>
+            <div className="hidden-lg-up">
+                <div className="dropdown">
+                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					Menu
+				</button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <Link to="/" className="nav link">Home</Link>
+                    <Link to="/bitcoin" className="nav link">BitCoin</Link>
+                    <Link to="/dash" className="nav link">DASH</Link>                    
+                    <Link to="/ethereum" className="nav link">Ethereum</Link>
+                    <Link to="/litecoin" className="nav link">LiteCoin</Link>                    
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
