8f4f496f1c977772acdf87b9c020940328daa956
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-22 12:03:21 -0600

    update routes to use helper functions

--- server/routes/btc-routes.js
+++ server/routes/btc-routes.js
@@ -1,12 +1,13 @@
 // import dependencies
 const express = require('express')
 const btcController = require('../controllers/btc-controller')
+const btcHelpers = require('../services/btc-helpers')
 
 // set router variable
 const btcRouter = express.Router()
 
 // set routes
 btcRouter.get('/', btcController.latest)
-btcRouter.post('/', btcController.create)
+btcRouter.post('/', btcHelpers.getCoinDesk, btcHelpers.getCapCoin, btcHelpers.getKraken, btcHelpers.getPolo, btcController.create)
 
 module.exports = btcRouter
