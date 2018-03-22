156860d04b0e7711a467048253701e255f474bf4
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-22 11:38:40 -0600

    added time made column to migrate fil

--- server/db/migrations/migration.sql
+++ server/db/migrations/migration.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS btc_data
 CREATE TABLE IF NOT EXISTS btc_data(
     id SERIAL PRIMARY KEY,
+    time_made BIGINT,
     usd DECIMAL,
     us_high DECIMAL,
     us_low DECIMAL,
@@ -16,6 +17,7 @@ CREATE TABLE IF NOT EXISTS btc_data(
 DROP TABLE IF EXISTS dash_data
 CREATE TABLE IF NOT EXISTS dash_data(
     id SERIAL PRIMARY KEY,
+    time_made BIGINT,
     usd DECIMAL,
     us_high DECIMAL,
     us_low DECIMAL,
@@ -31,6 +33,7 @@ CREATE TABLE IF NOT EXISTS dash_data(
 DROP TABLE IF EXISTS eth_data
 CREATE TABLE IF NOT EXISTS eth_data(
     id SERIAL PRIMARY KEY,
+    time_made BIGINT,
     usd DECIMAL,
     us_high DECIMAL,
     us_low DECIMAL,
@@ -46,6 +49,7 @@ CREATE TABLE IF NOT EXISTS eth_data(
 DROP TABLE IF EXISTS ltc_data
 CREATE TABLE IF NOT EXISTS ltc_data(
     id SERIAL PRIMARY KEY,
+    time_made BIGINT,
     usd DECIMAL,
     us_high DECIMAL,
     us_low DECIMAL,
