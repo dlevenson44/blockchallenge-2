fad5cad69bee9e3d7eeec5eb10700ad132f14020
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-23 13:14:40 -0400

    changed data types for migration file

--- server/db/migrations/migration.sql
+++ server/db/migrations/migration.sql
@@ -2,63 +2,63 @@ DROP TABLE IF EXISTS btc_data;
 CREATE TABLE IF NOT EXISTS btc_data(
     id SERIAL PRIMARY KEY,
     time_made BIGINT,
-    usd DECIMAL,
-    us_high DECIMAL,
-    us_low DECIMAL,
-    eur DECIMAL,
-    eur_high DECIMAL,
-    eur_low DECIMAL,
-    trades INT,
-    one_hour DECIMAL,
-    one_day DECIMAL,
-    one_week DECIMAL
+    usd TEXT,
+    us_high TEXT,
+    us_low TEXT,
+    eur TEXT,
+    eur_high TEXT,
+    eur_low TEXT,
+    trades TEXT,
+    one_hour TEXT,
+    one_day TEXT,
+    one_week TEXT
 );
 
 DROP TABLE IF EXISTS dash_data;
 CREATE TABLE IF NOT EXISTS dash_data(
     id SERIAL PRIMARY KEY,
     time_made BIGINT,
-    usd DECIMAL,
-    us_high DECIMAL,
-    us_low DECIMAL,
-    eur DECIMAL,
-    eur_high DECIMAL,
-    eur_low DECIMAL,
-    trades INT,
-    one_hour DECIMAL,
-    one_day DECIMAL,
-    one_week DECIMAL
+    usd TEXT,
+    us_high TEXT,
+    us_low TEXT,
+    eur TEXT,
+    eur_high TEXT,
+    eur_low TEXT,
+    trades TEXT,
+    one_hour TEXT,
+    one_day TEXT,
+    one_week TEXT
 );
 
 DROP TABLE IF EXISTS eth_data;
 CREATE TABLE IF NOT EXISTS eth_data(
     id SERIAL PRIMARY KEY,
     time_made BIGINT,
-    usd DECIMAL,
-    us_high DECIMAL,
-    us_low DECIMAL,
-    eur DECIMAL,
-    eur_high DECIMAL,
-    eur_low DECIMAL,
-    trades INT,
-    one_hour DECIMAL,
-    one_day DECIMAL,
-    one_week DECIMAL
+    usd TEXT,
+    us_high TEXT,
+    us_low TEXT,
+    eur TEXT,
+    eur_high TEXT,
+    eur_low TEXT,
+    trades TEXT,
+    one_hour TEXT,
+    one_day TEXT,
+    one_week TEXT
 );
 
 DROP TABLE IF EXISTS ltc_data;
 CREATE TABLE IF NOT EXISTS ltc_data(
     id SERIAL PRIMARY KEY,
     time_made BIGINT,
-    usd DECIMAL,
-    us_high DECIMAL,
-    us_low DECIMAL,
-    eur DECIMAL,
-    eur_high DECIMAL,
-    eur_low DECIMAL,
-    trades INT,
-    one_hour DECIMAL,
-    one_day DECIMAL,
-    one_week DECIMAL
+    usd TEXT,
+    us_high TEXT,
+    us_low TEXT,
+    eur TEXT,
+    eur_high TEXT,
+    eur_low TEXT,
+    trades TEXT,
+    one_hour TEXT,
+    one_day TEXT,
+    one_week TEXT
 );
 
