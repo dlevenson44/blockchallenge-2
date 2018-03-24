8faa2fc946294ddb1bf505745950560862d34106
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-24 16:12:16 -0400

    set up dashcontroller

--- client/src/components/DashController.jsx
+++ client/src/components/DashController.jsx
@@ -5,84 +5,110 @@ class DashController extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            usd: 0,
-            usHigh: 0,
-            usLow: 0,
-            eur: 0,
-            eurHigh: 0,
-            eurLow: 0,
-            trades: 0,
-            oneHour: 0,
-            oneDay: 0,
-            oneWeek: 0,
+            usd: '',
+            usHigh: '',
+            usLow: '',
+            eur: '',
+            eurHigh: '',
+            eurLow: '',
+            trades: '',
+            oneHour: '',
+            oneDay: '',
+            oneWeek: '',
             fetchStatus: false,
+            visited: false,
         }
-        this.getData = this.getData.bind(this)
+        // this.getData = this.getData.bind(this)
         this.renderData = this.renderData.bind(this)
+        this.sendToDb = this.sendToDb.bind(this)
     }
 
+    sendToDb() {        
+        if (this.props.fetchCounter === 10) {
+            fetch('/api/dash', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                body: JSON.stringify({
+                    usd: this.props.dashCapCoin.usd,
+                    us_high: this.props.dashPolo.high24hr,
+                    us_low: this.props.dashPolo.low24hr,
+                    eur: this.props.dashKraken.eur,
+                    eur_high: this.props.dashKraken.trends.high,
+                    eur_low: this.props.dashKraken.trends.low,
+                    trades: this.props.dashKraken.trends.trades,
+                    one_hour: this.props.dashCapCoin.trends.oneHour,
+                    one_day: this.props.dashCapCoin.trends.oneDay,
+                    one_week: this.props.dashCapCoin.trends.oneWeek,
+                }),
+            }).then(res => res.json())
+            .catch(err => console.log(err))
+        }
+        this.getData()
+    }
+
+    
     getData() {
+        console.log('GETTING')
         if (this.state.fetchStatus === false) {
-            // console.log('fetched')
-            fetch('/api/crypto')
+            fetch('/api/dash')
             .then(res => res.json())
             .then(res => {
+                console.log(res, 'RESPONSE YO')
+                // set state
                 this.setState({
-                    usd: res.data.crypto[0].dash_usd,
-                    usHigh: res.data.crypto[0].dash_us_high,
-                    usLow: res.data.crypto[0].dash_us_low,
-                    eur: res.data.crypto[0].dash_eur,
-                    eurHigh: res.data.crypto[0].dash_eur_high,
-                    eurLow: res.data.crypto[0].dash_eur_low,
-                    trades: res.data.crypto[0].dash_trades,
-                    oneHour: res.data.crypto[0].dash_one_hour,
-                    oneDay: res.data.crypto[0].dash_24_hours,
-                    oneWeek: res.data.crypto[0].dash_7_days,
+                    usd: res.data.dash[0].usd,
+                    usHigh: res.data.dash[0].us_high,
+                    usLow: res.data.dash[0].us_low,
+                    eur: res.data.dash[0].eur,
+                    eurHigh: res.data.dash[0].eur_high,
+                    eurLow: res.data.dash[0].eur_low,
+                    trades: res.data.dash[0].trades,
+                    oneHour: res.data.dash[0].one_hour,
+                    oneDay: res.data.dash[0].one_day,
+                    oneWeek: res.data.dash[0].one_week,
                     fetchStatus: true,
                 })
             })
             .catch(err => console.log(err))
         }
+        this.renderData()
     }
 
     renderData() {
-        if (this.state.fetchStatus === true) {
-            return(
-                <div className="crypto-container">
-                <h5>Trends:</h5>
-                    <p>{this.state.trades} trades in the last 24 hours</p>
-                    <p>{this.state.oneHour}% change in last hour</p>
-                    <p>{this.state.oneDay}% change in last 24 hours</p>
-                    <p>{this.state.oneWeek}% change in last 7 days</p>                
-                <h5>DASH US Market Info</h5>
-                    <p>${this.state.usd} per DASH</p>
-                    <p>${this.state.usHigh} is the 24 hour high</p>
-                    <p>${this.state.usLow} is the 24 hour low</p>                            
-                <h5>DASH EU Market Info</h5>
-                    <p>€{this.state.eur} per DASH</p>
-                    <p>€{this.state.eurHigh} is the 24 hour high</p>
-                    <p>€{this.state.eurLow} is the 24 hour low</p>            
-            </div>
-            )
+        return(
+            <div className="crypto-container">
+            <h5>Trends:</h5>
+                <p>{this.state.trades} trades in the last 24 hours</p>
+                <p>{this.state.oneHour}% change in last hour</p>
+                <p>{this.state.oneDay}% change in last 24 hours</p>
+                <p>{this.state.oneWeek}% change in last 7 days</p>                
+            <h5>DASH US Market Info</h5>
+                <p>${this.state.usd} per DASH</p>
+                <p>${(this.state.usHigh).substring(0, 8)} is the 24 hour high</p>
+                <p>${(this.state.usLow).substring(0, 8)} is the 24 hour low</p>                            
+            <h5>DASH EU Market Info</h5>
+                <p>€{(this.state.eur).substring(0, 8)} per DASH</p>
+                <p>€{(this.state.eurHigh).substring(0, 8)} is the 24 hour high</p>
+                <p>€{(this.state.eurLow).substring(0, 8)} is the 24 hour low</p>            
+        </div>
 
-        } else {
-            return(
-                <div>
-                    <p>Loading Data</p>
-                </div>
-            )
-        }
+        )
     }
 
     render() {
-        this.getData()
-        // console.log(this.state)
+        this.sendToDb()
+        
         return(
             <div className="crypto-container">
-                {this.renderData()}
+                {this.renderData()}                
             </div>
         )
     }
 }
 
 export default DashController
+
+
+
