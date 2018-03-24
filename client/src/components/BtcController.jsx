fb747b9b33dc78393d97102263219fca9424d8cf
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-24 15:49:11 -0400

    POST data from props in btccontroller, set state to newly fetched data

--- client/src/components/BtcController.jsx
+++ client/src/components/BtcController.jsx
@@ -19,13 +19,12 @@ class BtcController extends Component {
             visited: false,
         }
         // this.getData = this.getData.bind(this)
-        // this.renderData = this.renderData.bind(this)
+        this.renderData = this.renderData.bind(this)
         this.sendToDb = this.sendToDb.bind(this)
     }
 
     sendToDb() {        
         if (this.props.fetchCounter === 10) {
-            console.log(this.props)
             fetch('/api/btc', {
                 headers: {
                     'Content-Type': 'application/json',
@@ -46,15 +45,65 @@ class BtcController extends Component {
             }).then(res => res.json())
             .catch(err => console.log(err))
         }
+        this.getData()
     }
+
+    getData() {
+        console.log('GETTING')
+        if (this.state.fetchStatus === false) {
+            fetch('/api/btc')
+            .then(res => res.json())
+            .then(res => {
+                // set state
+                this.setState({
+                    usd: res.data.btc[0].usd,
+                    usHigh: res.data.btc[0].us_high,
+                    usLow: res.data.btc[0].us_low,
+                    eur: res.data.btc[0].eur,
+                    eurHigh: res.data.btc[0].eur_high,
+                    eurLow: res.data.btc[0].eur_low,
+                    trades: res.data.btc[0].trades,
+                    oneHour: res.data.btc[0].one_hour,
+                    oneDay: res.data.btc[0].day,
+                    oneWeek: res.data.btc[0].one_week,
+                    fetchStatus: true,
+                })
+            })
+            .catch(err => console.log(err))
+        }
+        this.renderData()
+    }
+
+
+    // return(
+    //     <div>
+    //         <p>Loading Data</p>
+    //     </div>
+    // )
+
+
+
+    renderData() {
+        console.log(this)
+            return(
+                <div>
+                    <h1>{this.state.usd}</h1>
+                </div>
+
+            )
+
+
+     
+    }
+
+
 
     render() {
         this.sendToDb()
-        // {this.renderData()}
-        // console.log(this.props)
+        
         return(
             <div className="crypto-container">
-                <h1>hello</h1>
+                {this.renderData()}                
             </div>
         )
     }
@@ -63,57 +112,4 @@ class BtcController extends Component {
 export default BtcController
 
 
-    // getData() {
-    //     if (this.state.fetchStatus === false) {
-    //         fetch('/api/crypto')
-    //         .then(res => res.json())
-    //         .then(res => {
-    //             // set state
-    //             this.setState({
-    //                 usd: res.data.crypto[0].btc_usd,
-    //                 usHigh: res.data.crypto[0].btc_us_high,
-    //                 usLow: res.data.crypto[0].btc_us_low,
-    //                 eur: res.data.crypto[0].btc_eur,
-    //                 eurHigh: res.data.crypto[0].btc_eur_high,
-    //                 eurLow: res.data.crypto[0].btc_eur_low,
-    //                 trades: res.data.crypto[0].btc_trades,
-    //                 oneHour: res.data.crypto[0].btc_one_hour,
-    //                 oneDay: res.data.crypto[0].btc_24_hours,
-    //                 oneWeek: res.data.crypto[0].btc_7_days,
-    //                 fetchStatus: true,
-    //             })
-    //         })
-    //         .catch(err => console.log(err))
-    //     }
-    //     this.sendToDb()
-    // }
 
-    // renderData() {
-    //     if (this.state.fetchStatus === true) {
-    //         return(
-    //             <div className="crypto-container">
-    //             <h5>Trends:</h5>
-    //                 <p>{this.state.trades} trades in the last 24 hours</p>
-    //                 <p>{this.state.oneHour}% change in last hour</p>
-    //                 <p>{this.state.oneDay}% change in last 24 hours</p>
-    //                 <p>{this.state.oneWeek}% change in last 7 days</p>                
-    //             <h5>BTC US Market Info</h5>
-    //                 <p>${this.state.usd} per BTC</p>
-    //                 <p>${(this.state.usHigh).substring(0, 8)} is the 24 hour high</p>
-    //                 <p>${(this.state.usLow).substring(0, 8)} is the 24 hour low</p>                            
-    //             <h5>BTC EU Market Info</h5>
-    //                 <p>€{(this.state.eur).substring(0, 8)} per BTC</p>
-    //                 <p>€{(this.state.eurHigh).substring(0, 8)} is the 24 hour high</p>
-    //                 <p>€{(this.state.eurLow).substring(0, 8)} is the 24 hour low</p>            
-    //         </div>
-    //         )
-
-    //     } else {
-    //         return(
-    //             <div>
-    //                 <p>Loading Data</p>
-    //             </div>
-    //         )
-    //     }
-        
-    // }
