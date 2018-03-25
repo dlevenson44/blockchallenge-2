93e3bc32d7b0d4f3d296d8fdd0d6652e05ed0aab
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-25 16:58:30 -0400

    created and set up eth-controller

---
+++ server/controllers/eth-controller.js
@@ -0,0 +1,68 @@
+// import model
+const Eth = require('../models/Eth')
+
+// initiate controller object
+const ethController = {}
+
+// pull all entries from table
+ethController.index = (req, res, next) => {
+    th.findAll()
+    .then(crypto => {
+        res.json({
+            message: 'retrieved data',
+            data: { crypto }
+        })
+    })
+    .catch(next)
+}
+
+// find latest entry
+ethController.latest = (req, res, next) => {
+    th.findRecent()
+    .then(crypto => {
+        res.json({
+            message: 'retrieved entry',
+            data: { crypto }
+        })
+    }).catch(next)
+}
+
+// send api data
+ethController.sendApiData = (req, res) => {
+    res.json({
+        message: 'data returned for ETH',
+        ethCapCoin: res.locals.ethCapCoin,
+        ethKraken: res.locals.ethKraken,
+        ethPolo: res.locals.ethPolo
+    })
+}
+
+// create new entry
+ethController.create = (req, res) => {
+    console.log(req.body, ' req.body from ethController#create')
+    Eth.create({
+        // time made
+        time_made: Date.now(),
+        // eth data
+        usd: req.body.usd,
+        us_high: req.body.us_high,
+        us_low: req.body.us_low,
+        eur: req.body.eur,
+        eur_high: req.body.eur_high,
+        eur_low: req.body.eur_low,
+        trades: req.body.trades,
+        one_hour: req.body.one_hour,
+        one_day: req.body.one_day,
+        seven_days: req.body.Seven_days,
+    })
+}
+
+ethController.index = (req, res) => {
+    res.render('index', {
+        ethCapCoin: res.locals.ethCapCoin,
+        ethKraken: res.locals.ethKraken,
+        ethPolo: res.locals.ethPolo
+    })
+}
+
+module.exports = ethController
