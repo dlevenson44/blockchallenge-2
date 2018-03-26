2f398458817601c054de13922fc1ad2100af1c46
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-26 15:02:56 -0400

    fixed top level component API calls

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      getTimer: false,
+      puller: false,
       btc: {
         usd: '',
         usHigh: '',
@@ -56,17 +56,47 @@ class App extends Component {
         trades: ''
       },
     }
+    this.fetchBtc = this.fetchBtc.bind(this)
+  }
+
+  componentWillMount() {
+    this.fetchBtc()
   }
 
   fetchBtc() {
-    fetch('http://localhost:3001/btc', {
+      fetch('/btc', {
+        method: 'GET',
+        mode: 'no-cors',
+      }).then(res => res.json())
+      this.fetchDash()
+  }
+
+  fetchDash() {
+    fetch('/dash', {
       method: 'GET',
+      mode: 'no-cors',
+    }).then(res => res.json())
+    this.fetchEth()
+  }
+
+  fetchEth() {
+    fetch('/eth', {
+      method: 'GET',
+      mode: 'no-cors',
+    }).then(res => res.json())
+    this.fetchLtc()
+  }
+
+  fetchLtc() {
+    fetch('/ltc', {
+      method: 'GET',
+      mode: 'no-cors',
     }).then(res => res.json())
     this.getBtc()
   }
 
   getBtc() {
-    if (this.state.getTimer === false) {
+    if (this.state.puller === false) {
     fetch('/api/btc', {
       method: 'GET',
     }).then(res => res.json())
@@ -84,16 +114,84 @@ class App extends Component {
           oneWeek: res.data.crypto[0].one_week,
           trades: res.data.crypto[0].trades
         },
-        getTimer: true
+        puller: true
       })
     })
-    console.log('set state')
+    this.getDash()
     }
+  }
+
+  getDash() {
+    fetch('/api/dash', {
+      method: 'GET',
+    }).then(res => res.json())
+    .then(res => {
+      console.log(res)
+      this.setState({
+        dash: {
+          usd: res.data.crypto[0].usd,
+          usHigh: res.data.crypto[0].us_high,
+          usLow: res.data.crypto[0].us_low,
+          eur: res.data.crypto[0].eur,
+          eurHigh: res.data.crypto[0].eur_high,
+          eurLow: res.data.crypto[0].eur_low,
+          oneHour: res.data.crypto[0].one_hour,
+          oneDay: res.data.crypto[0].one_day,
+          oneWeek: res.data.crypto[0].one_week,
+          trades: res.data.crypto[0].trades
+        }
+      })
+    })
+    this.getEth()
+  }
+
+  getEth() {
+    fetch('/api/eth', {
+      method: 'GET',
+    }).then(res => res.json())
+    .then(res => {
+      this.setState({
+        eth: {
+          usd: res.data.crypto[0].usd,
+          usHigh: res.data.crypto[0].us_high,
+          usLow: res.data.crypto[0].us_low,
+          eur: res.data.crypto[0].eur,
+          eurHigh: res.data.crypto[0].eur_high,
+          eurLow: res.data.crypto[0].eur_low,
+          oneHour: res.data.crypto[0].one_hour,
+          oneDay: res.data.crypto[0].one_day,
+          oneWeek: res.data.crypto[0].one_week,
+          trades: res.data.crypto[0].trades
+        }
+      })
+    })
+    this.getLtc()
+  }
+
+  getLtc() {
+    fetch('/api/ltc', {
+      method: 'GET',
+    }).then(res => res.json())
+    .then(res => {
+      this.setState({
+        ltc: {
+          usd: res.data.crypto[0].usd,
+          usHigh: res.data.crypto[0].us_high,
+          usLow: res.data.crypto[0].us_low,
+          eur: res.data.crypto[0].eur,
+          eurHigh: res.data.crypto[0].eur_high,
+          eurLow: res.data.crypto[0].eur_low,
+          oneHour: res.data.crypto[0].one_hour,
+          oneDay: res.data.crypto[0].one_day,
+          oneWeek: res.data.crypto[0].one_week,
+          trades: res.data.crypto[0].trades
+        }
+      })
+    })
   }
 
 
   render() {
-    this.fetchBtc()
     return (
       <div className="App">
         <header className="App-header">
