bdb0200d02f7ae904cea991e8e4eec3803c0ed62
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-26 12:29:54 -0400

    updated find recent model function

--- server/models/Btc.js
+++ server/models/Btc.js
@@ -11,8 +11,13 @@ Btc.findAll = () => {
 
 // find most recent entry
 Btc.findRecent = () => {
-    return db.query(`SELECT * FROM tracked_data ORDER BY id DESC LIMIT 1`)
+    return db.query(`
+        SELECT usd, one_hour, one_day, one_week, eur,
+        eur_low, eur_high, trades, us_high, us_low
+        FROM cap_coin, kraken, polo
+        WHERE cap_coin.crypto_id = kraken.crypto_id
+        ORDER BY cap_coin.id DESC LIMIT 1
+    `)
 }
-
 
 module.exports = Btc
