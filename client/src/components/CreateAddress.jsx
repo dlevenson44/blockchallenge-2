e591665ee3b08d7935ef71e55df1f571abd4c228
Daniel Levenson <daniellevenson4@gmail.com>
2018-03-28 16:29:31 -0400

    finalized createaddress component

--- client/src/components/CreateAddress.jsx
+++ client/src/components/CreateAddress.jsx
@@ -10,25 +10,40 @@ class CreateAddress extends Component {
            address: '',
            wif: ''
         }
+        this.createAddress = this.createAddress.bind(this)
     }
 
     componentWillMount() {
-
+        this.createAddress()
     }
 
-    createAddress() {
+    createAddress(e) {
         fetch('https://api.blockcypher.com/v1/btc/test3/addrs', {
             method: 'POST',
         }).then(res => res.json())
         .then(res => {
-            console.log(res)
-        })
+            this.setState({
+                private: res.private,
+                public: res.public,
+                address: res.address,
+                wif: res.wif
+            })
+        })       
     }
+
+
 
     render() {
         return(
             <div>
-                <p>address component</p>
+                <h3>New BitCoin Address Info</h3>
+                <p id="add-info">Below is the info for your newly created BitCoin address.  Please write this information down for your records, as we do not store any newly created address information.</p>
+                <p id="add-info">To create a new address, simply refresh the page and the information below will be updated accordingly.</p>
+                <p>Address:  {this.state.address} </p>                
+                <p>Public Key:   {this.state.public}   </p>
+                <p>Private Key (Hex Encoded):  {this.state.private}  </p>
+                <p>Private Key (Wif Encoded):   {this.state.wif} </p>
+                
             </div>
         )
     }
