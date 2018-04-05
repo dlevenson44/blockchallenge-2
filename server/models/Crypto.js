bf8ed8a38138952b777b542a0a9455dbc389ea70
Daniel Levenson <daniellevenson4@gmail.com>
2018-04-05 01:04:07 -0400

    added create function to crypto model

--- server/models/Crypto.js
+++ server/models/Crypto.js
@@ -4,6 +4,29 @@ const db = require('../db/config')
 // initiate model object
 const Crypto = {}
 
+// create new entry
+Crypto.create = (crypto) => {
+    return db.one(`
+        INSERT INTO tracked_data
+        (crypto_id, time_made, trades, one_hour, one_day, one_week, usd, us_high, us_low, eur, eur_high, eur_low)
+        VALUES
+        (
+            $1,
+            $2,
+            $3,
+            $4,
+            $5,
+            $6,
+            $7,
+            $8,
+            $9,
+            $10,
+            $11,
+            $12
+        ) RETURNING *
+    `, [crypto.crypto_id, Date.now(), crypto.trades, crypto.one_hour, crypto.one_day, crypto.one_week, crypto.usd, crypto.us_high, crypto.us_low, crypto.eur, crypto.eur_high, crypto.eur_low])
+}
+
 // find most recent BTC entry
 Crypto.findRecent = () => {
     return db.query(`
