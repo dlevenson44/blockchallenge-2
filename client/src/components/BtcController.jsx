680c924d80ebe559d40a90223fa9259602c63648
Daniel Levenson <daniellevenson4@gmail.com>
2018-04-05 01:14:14 -0400

    updated btc controller component

--- client/src/components/BtcController.jsx
+++ client/src/components/BtcController.jsx
@@ -1,23 +1,91 @@
-import React from 'react'
+// import dependency
+import React, { Component } from 'react';
 
-const BtcController = (props) => {
-    return(
-    <div className="crypto-container">
-        <h5>Trends:</h5>
-            <p>{(props.btc.trades).substring(0, 5)} trades in the last 24 hours</p>
-            <p>{props.btc.oneHour}% change in last hour</p>
-            <p>{props.btc.oneDay}% change in last 24 hours</p>
-            <p>{props.btc.oneWeek}% change in last 7 days</p>                
-        <h5>BTC US Market Info</h5>
-            <p>${(props.btc.usd).substring(0, 8)} per BTC</p>
-            <p>${(props.btc.usHigh).substring(0, 8)} is the 24 hour high</p>
-            <p>${(props.btc.usLow).substring(0, 8)} is the 24 hour low</p>                            
-        <h5>BTC EU Market Info</h5>
-            <p>€{(props.btc.eur).substring(0, 8)} per BTC</p>
-            <p>€{(props.btc.eurHigh).substring(0, 8)} is the 24 hour high</p>
-            <p>€{(props.btc.eurLow).substring(0, 8)} is the 24 hour low</p>            
-    </div>
-    )
+class BtcController extends Component {
+    constructor(props) {
+        super(props)
+        console.log(this.props)
+        this.state = {
+            posted: false,
+        }
+        // bind functions
+        this.renderData = this.renderData.bind(this)
+        this.sendToDb = this.sendToDb.bind(this)
+    }
+
+    sendToDb() {   
+        //  do not sent to DB until all API calls are ran, prevent 0 values from being entered
+        console.log(this.props.fetched)
+        if ((this.props.fetched === true)) {
+            fetch('/crypto/btc', {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                method: 'POST',
+                // convert props to JSON string
+                body: JSON.stringify({
+                    usd: this.props.btc.usd,
+                    us_high: this.props.btc.usHigh,
+                    us_low: this.props.btc.usLow,
+                    eur: this.props.btc.eur,
+                    eur_high: this.props.btc.eurHigh,
+                    eur_low: this.props.btc.eurLow,
+                    trades: this.props.btc.trades,
+                    one_hour: this.props.btc.oneHour,
+                    one_day: this.props.btc.oneDay,
+                    one_week: this.props.btc.oneWeek,
+                    crypto_id: 1
+                }),
+            // send JSON response
+            }).then(res => res.json())
+            // catch errors
+            .catch(err => console.log(err))
+        }
+    }
+
+    renderData() {
+        // wait for state to convert to string type to clean up
+        // if (typeof this.state.usd === 'string') {
+        // return(
+        //     <div className="crypto-container">
+            // <h5>Trends:</h5>
+            //     <p>{this.state.trades} trades in the last 24 hours</p>
+            //     <p>{this.state.oneHour}% change in last hour</p>
+            //     <p>{this.state.oneDay}% change in last 24 hours</p>
+            //     <p>{this.state.oneWeek}% change in last 7 days</p>                
+            // <h5>BTC US Market Info</h5>
+            //     <p>${(this.state.usd).substring(0, 8)} per BTC</p>
+            //     <p>${(this.state.usHigh).substring(0, 8)} is the 24 hour high</p>
+            //     <p>${(this.state.usLow).substring(0, 8)} is the 24 hour low</p>                            
+            // <h5>BTC EU Market Info</h5>
+            //     <p>€{(this.state.eur).substring(0, 8)} per BTC</p>
+            //     <p>€{(this.state.eurHigh).substring(0, 8)} is the 24 hour high</p>
+            //     <p>€{(this.state.eurLow).substring(0, 8)} is the 24 hour low</p>            
+        // </div>
+        // )
+        // } 
+    }
+
+    render() {
+        this.sendToDb()        
+        return(
+            <div className="crypto-container">
+            <h5>Trends:</h5>
+                <p>{this.props.btc.trades} trades in the last 24 hours</p>
+                <p>{this.props.btc.oneHour}% change in last hour</p>
+                <p>{this.props.btc.oneDay}% change in last 24 hours</p>
+                <p>{this.props.btc.oneWeek}% change in last 7 days</p>                
+            <h5>BTC US Market Info</h5>
+                <p>${(this.props.btc.usd).substring(0, 8)} per BTC</p>
+                <p>${(this.props.btc.usHigh).substring(0, 8)} is the 24 hour high</p>
+                <p>${(this.props.btc.usLow).substring(0, 8)} is the 24 hour low</p>                            
+            <h5>BTC EU Market Info</h5>
+                <p>€{(this.props.btc.eur).substring(0, 8)} per BTC</p>
+                <p>€{(this.props.btc.eurHigh).substring(0, 8)} is the 24 hour high</p>
+                <p>€{(this.props.btc.eurLow).substring(0, 8)} is the 24 hour low</p>         
+            </div>
+        )
+    }
 }
 
 export default BtcController
