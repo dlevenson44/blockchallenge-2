0f8b017a748f7e0e6e6bdb083b839f5d7ab16ebd
Daniel Levenson <daniellevenson4@gmail.com>
2018-04-05 00:22:54 -0400

    rendering all data in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
     }
     // bind functions
     this.fetchData = this.fetchData.bind(this)
-    // this.renderChart = this.renderChart.bind(this)
+    this.renderChart = this.renderChart.bind(this)
   }
 
   componentWillMount() {
@@ -180,35 +180,22 @@ class App extends Component {
     }    
   }
 
-  // renderChart() {
-  //   // render chart after alt per btc after it's been calculated
-  //   if (this.state.calculated === true) {
-  //     return(
-  //       <div>
-  //         <BtcChart alt={this.state.alt} />
-  //       </div>
-  //     )
-  //   } else {
-  //     return(
-  //       <div>
-  //         <p>loading</p>
-  //       </div>
-  //     )
-  //   }
-  // }
-
-  // <AltController btc={this.state.btc.usd} dash={this.state.dash.usd}
-  // eth={this.state.eth.usd} ltc={this.state.ltc.usd} />
-
-  // <div>
-  // <Route exact path='/' render={() => <ChartController renderChart={this.renderChart()} /> } />
-  // <Route path='/bitcoin' render={() => <BtcController btc={this.state.btc} /> } />
-  // <Route path='/dash' render={() => <DashController dash={this.state.dash} /> } />
-  // <Route path='/ethereum' render={() => <EthController eth={this.state.eth} /> } />
-  // <Route path='/litecoin' render={() => <LtcController ltc={this.state.ltc} /> } />
-  // <Route path='/create' render={() => <CreateAddress /> } />
-  // </div>
-
+  renderChart() {
+    // render chart after alt per btc after it's been calculated
+    if (this.state.calculated === true) {
+      return(
+        <div>
+          <BtcChart alt={this.state.alt} />
+        </div>
+      )
+    } else {
+      return(
+        <div>
+          <p>loading</p>
+        </div>
+      )
+    }
+  }
 
   render() {
     this.calculateData()
@@ -218,8 +205,16 @@ class App extends Component {
         <div className="container">
         <h1>Crypto Tracker</h1>
         <Nav />
-        <p>altcontroller component goes here</p>
-        <p>route div goes here</p>
+        <AltController btc={this.state.btc.usd} dash={this.state.dash.usd}
+          eth={this.state.eth.usd} ltc={this.state.ltc.usd} />
+          <div>
+            <Route exact path='/' render={() => <ChartController renderChart={this.renderChart()} /> } />
+            <Route path='/bitcoin' render={() => <BtcController btc={this.state.btc} /> } />
+            <Route path='/dash' render={() => <DashController dash={this.state.dash} /> } />
+            <Route path='/ethereum' render={() => <EthController eth={this.state.eth} /> } />
+            <Route path='/litecoin' render={() => <LtcController ltc={this.state.ltc} /> } />
+            <Route path='/create' render={() => <CreateAddress /> } />
+          </div>
         </div>
         </div>
       </Router>
@@ -228,147 +223,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
-
-// fetch BTC data from API
-  // fetchBtc() {
-  //     fetch('/btc', {
-  //       method: 'GET',
-  //       mode: 'no-cors',
-  //     }).then(res => res.json())
-  //     this.fetchDash()
-  // }
-
-  // // fetch DASH data from API
-  // fetchDash() {
-  //   fetch('/dash', {
-  //     method: 'GET',
-  //     mode: 'no-cors',
-  //   }).then(res => res.json())
-  //   this.fetchEth()
-  // }
-
-  // // fetch ETH data from API
-  // fetchEth() {
-  //   fetch('/eth', {
-  //     method: 'GET',
-  //     mode: 'no-cors',
-  //   }).then(res => res.json())
-  //   this.fetchLtc()
-  // }
-
-  // // fetch LTC data from API
-  // fetchLtc() {
-  //   fetch('/ltc', {
-  //     method: 'GET',
-  //     mode: 'no-cors',
-  //   }).then(res => res.json())
-  //   this.getBtc()
-  // }
-
-  // // set BTC data to state
-  // getBtc() {
-  //   fetch('/api/btc', {
-  //     method: 'GET',
-  //   }).then(res => res.json())
-  //   .then(res => {
-  //     let fetch = (this.state.fetcher + 1)
-  //     this.setState({
-  //       fetcher: fetch,
-  //       btc: {
-  //         usd: res.data.crypto[0].usd,
-  //         usHigh: res.data.crypto[0].us_high,
-  //         usLow: res.data.crypto[0].us_low,
-  //         eur: res.data.crypto[0].eur,
-  //         eurHigh: res.data.crypto[0].eur_high,
-  //         eurLow: res.data.crypto[0].eur_low,
-  //         oneHour: res.data.crypto[0].one_hour,
-  //         oneDay: res.data.crypto[0].one_day,
-  //         oneWeek: res.data.crypto[0].one_week,
-  //         trades: res.data.crypto[0].trades
-  //       },
-  //       counter: 0,
-  //     })
-  //   })
-  //   this.getDash()
-  // }
-
-  // // set DASH data to state
-  // getDash() {
-  //   fetch('/api/dash', {
-  //     method: 'GET',
-  //   }).then(res => res.json())
-  //   .then(res => {
-  //     let fetch = (this.state.fetcher + 1)
-  //     this.setState({
-  //       fetcher: fetch,
-  //       dash: {
-  //         usd: res.data.crypto[0].usd,
-  //         usHigh: res.data.crypto[0].us_high,
-  //         usLow: res.data.crypto[0].us_low,
-  //         eur: res.data.crypto[0].eur,
-  //         eurHigh: res.data.crypto[0].eur_high,
-  //         eurLow: res.data.crypto[0].eur_low,
-  //         oneHour: res.data.crypto[0].one_hour,
-  //         oneDay: res.data.crypto[0].one_day,
-  //         oneWeek: res.data.crypto[0].one_week,
-  //         trades: res.data.crypto[0].trades
-  //       }
-  //     })
-  //   })
-  //   this.getEth()
-  // }
-  
-  // // set ETH data to state
-  // getEth() {
-  //   fetch('/api/eth', {
-  //     method: 'GET',
-  //   }).then(res => res.json())
-  //   .then(res => {
-  //     let fetch = (this.state.fetcher + 1)
-  //     this.setState({
-  //       fetcher: fetch,
-  //       eth: {
-  //         usd: res.data.crypto[0].usd,
-  //         usHigh: res.data.crypto[0].us_high,
-  //         usLow: res.data.crypto[0].us_low,
-  //         eur: res.data.crypto[0].eur,
-  //         eurHigh: res.data.crypto[0].eur_high,
-  //         eurLow: res.data.crypto[0].eur_low,
-  //         oneHour: res.data.crypto[0].one_hour,
-  //         oneDay: res.data.crypto[0].one_day,
-  //         oneWeek: res.data.crypto[0].one_week,
-  //         trades: res.data.crypto[0].trades
-  //       }
-  //     })
-  //   })
-  //   this.getLtc()
-  // }
-
-  // // set LTC data to state
-  // getLtc() {
-  //   fetch('/api/ltc', {
-  //     method: 'GET',
-  //   }).then(res => res.json())
-  //   .then(res => {
-  //     let fetch = (this.state.fetcher + 1)
-  //     this.setState({
-  //       fetcher: fetch,
-  //       ltc: {
-  //         usd: res.data.crypto[0].usd,
-  //         usHigh: res.data.crypto[0].us_high,
-  //         usLow: res.data.crypto[0].us_low,
-  //         eur: res.data.crypto[0].eur,
-  //         eurHigh: res.data.crypto[0].eur_high,
-  //         eurLow: res.data.crypto[0].eur_low,
-  //         oneHour: res.data.crypto[0].one_hour,
-  //         oneDay: res.data.crypto[0].one_day,
-  //         oneWeek: res.data.crypto[0].one_week,
-  //         trades: res.data.crypto[0].trades
-  //       }
-  //     })
-  //   })    
-  // }
